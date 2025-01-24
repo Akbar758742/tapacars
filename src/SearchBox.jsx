c84779be87e5e9e9d1ccaa5db9a39e67aba31e46
akbar hossen <mdakbar758742@gmail.com>
2025-01-23 21:02:52 -0800

    searchborder

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,7 +3,7 @@
 const SearchBox = () => {
     return (
         <div className="flex items-center m-2">
-            <input type="text" placeholder="Search cars..." className="mr-4" />
+            <input type="text" placeholder="Search cars..." className="mr-4 border-2 rounded-sm" />
             <label for="premium-only">
                 <input type="checkbox" id="premium-only" />
                 Show Premium Only
